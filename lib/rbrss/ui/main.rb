2fee05a3a641184da031e143748f46631af2132d
Pascal Terjan <pterjan@linuxfr.org>
2009-07-13 16:12:04 +0200

    Uncomment auto_save handling

--- lib/rbrss/ui/main.rb
+++ lib/rbrss/ui/main.rb
@@ -284,7 +284,7 @@ module RbRSS
         rescue REXML::ParseException
           puts "Invalid XML : "+s
         end
-        #	on_save1_activate(nil) if(@conf["auto_save"])
+        on_save1_activate(nil) if(@conf["auto_save"])
       end
 
       ##############
